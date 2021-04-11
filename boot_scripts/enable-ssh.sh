db17ac23a8e817096f37094ba5994e73733b5fb3
utky <ilyaletre@gmail.com>
2021-04-11 22:18:15 +0900

    succeeded to build

--- boot_scripts/enable-ssh.sh
+++ boot_scripts/enable-ssh.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -x
 
-PASSWORD=$(/usr/bin/openssl passwd -crypt "$1")
+USERNAME=$1
+PASSWORD=$2
+PUBLIC_KEY=$3
 
-# Vagrant-specific configuration
-/usr/bin/useradd --password ${PASSWORD} --comment 'Packer User' --create-home --user-group packer
-echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_packer
-echo 'packer ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_packer
-/usr/bin/chmod 0440 /etc/sudoers.d/10_packer
+ENCRYPTED_PASSWORD=$(/usr/bin/openssl passwd -crypt "$PASSWORD")
+/usr/bin/useradd --password ${ENCRYPTED_PASSWORD} --create-home --user-group $USERNAME
+mkdir -p /home/$USERNAME/.ssh
+chmod 700 /home/$USERNAME/.ssh
+echo "${PUBLIC_KEY}" >> /home/$USERNAME/.ssh/authorized_keys
+chmod 600 /home/$USERNAME/.ssh/authorized_keys
+chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_${USERNAME}
+echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10_${USERNAME}
+/usr/bin/chmod 0440 /etc/sudoers.d/10_${USERNAME}
 /usr/bin/systemctl start sshd.service
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,18 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 # https://wiki.archlinux.org/index.php/installation_guide#Pre-installation
 
 timedatectl set-ntp true
 
 # Partitioning
 ROOT_DEVICE=/dev/sda
+FS_TYPE=ext4
 FS_PART_NUM=3
 FS_DEVICE=${ROOT_DEVICE}${FS_PART_NUM}
-VG_NAME="primary"
-LV_NAME="root"
-LV_DEVICE="/dev/${VG_NAME}/${LV_NAME}"
 CONFIG_SCRIPT="/usr/local/bin/arch-config.sh"
 CONFIG_SCRIPT_SHORT=$(basename ${CONFIG_SCRIPT})
 FQDN=workspace
 KEYMAP=us
 LANGUAGE=en_US.UTF-8
-DATA_DISK_UUID=${DATA_DISK_UUID:-$DATA_DISK_UUID}
-MAIN_USER=ilyaletre
 
 sgdisk -o ${ROOT_DEVICE}
 
@@ -24,36 +20,23 @@ sgdisk --new=1:0:+512M --change-name=1:"boot" --typecode=1:EF00 ${ROOT_DEVICE}
 sgdisk --new=2:0:+1G --change-name=2:"swap" --typecode=2:8200 ${ROOT_DEVICE}
 sgdisk --new=${FS_PART_NUM}:0:0 --change-name=${FS_PART_NUM}:"filesystem" --typecode=${FS_PART_NUM}:8300 ${ROOT_DEVICE}
 sgdisk -p ${ROOT_DEVICE}
-
-# LVM provisioning
-pvcreate "${FS_DEVICE}"
-pvs
-
-vgcreate "${VG_NAME}" "${FS_DEVICE}"
-vgs
-
-lvcreate --extents +100%FREE "${VG_NAME}" --name "${LV_NAME}"
-
 lsblk
 
-ls -l /dev/disk/by-uuid
-
 # Formatting
-mkfs.vfat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 "${LV_DEVICE}"
-mount "${LV_DEVICE}" /mnt
+mkfs.vfat -F32 ${ROOT_DEVICE}1
+mkswap ${ROOT_DEVICE}2
+swapon ${ROOT_DEVICE}2
+mkfs.${FS_TYPE} "${FS_DEVICE}"
+mount "${FS_DEVICE}" /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir -p /mnt/mnt/data
-mount --uuid "${DATA_DISK_UUID}" /mnt/mnt/data
+mount ${ROOT_DEVICE}1 /mnt/boot
 
 # Installation
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cat <<-EOF > "/mnt${CONFIG_SCRIPT}"
+  set -x
   echo ">>>> ${CONFIG_SCRIPT}: Configuring hostname, timezone, and keymap.."
   echo '${FQDN}' > /etc/hostname
   /usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
@@ -71,28 +54,29 @@ cat <<-EOF > "/mnt${CONFIG_SCRIPT}"
   # Disable systemd Predictable Network Interface Names and revert to traditional interface names
   # https://wiki.archlinux.org/index.php/Network_configuration#Revert_to_traditional_interface_names
   #/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+  #pacman -S --noconfirm dhcpd
   #/usr/bin/systemctl enable dhcpcd@eth0.service
 
-  #echo ">>>> ${CONFIG_SCRIPT_SHORT}: Configuring sshd.."
+  echo ">>>> ${CONFIG_SCRIPT_SHORT}: Configuring sshd.."
   #/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
-  #/usr/bin/systemctl enable sshd.service
+  pacman -S --noconfirm openssh
+  /usr/bin/systemctl enable sshd.service
   # Workaround for https://bugs.archlinux.org/task/58355 which prevents sshd to accept connections after reboot
-
   #echo ">>>> ${CONFIG_SCRIPT_SHORT}: Adding workaround for sshd connection issue after reboot.."
   #/usr/bin/pacman -S --noconfirm rng-tools
   #/usr/bin/systemctl enable rngd
 
-  # Vagrant-specific configuration
+  # Main-user configuration
+  pacman -S --noconfirm sudo
   echo ">>>> ${CONFIG_SCRIPT_SHORT}: Creating main user.."
-
-  /usr/bin/useradd --password ${PASSWORD} --comment 'Main User' --create-home --user-group "${MAIN_USER}"
+  /usr/bin/useradd --comment 'Main User' --password $(/usr/bin/openssl passwd -crypt "${MAIN_PASSWORD}") --create-home --user-group "${MAIN_USER}"
   echo ">>>> ${CONFIG_SCRIPT_SHORT}: Configuring sudo.."
-  echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_${MAIN_USER}
-  echo "${MAIN_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10_${MAIN_USER}
-  /usr/bin/chmod 0440 /etc/sudoers.d/10_${MAIN_USER}
+  echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/00_${MAIN_USER}
+  echo "${MAIN_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/00_${MAIN_USER}
+  /usr/bin/chmod 0640 /etc/sudoers.d/00_${MAIN_USER}
   echo ">>>> ${CONFIG_SCRIPT_SHORT}: Configuring ssh access for ${MAIN_USER}.."
   /usr/bin/install --directory --owner=${MAIN_USER} --group=${MAIN_USER} --mode=0700 /home/${MAIN_USER}/.ssh
-  /usr/bin/curl --output /home/${MAIN_USER}/.ssh/authorized_keys --location https://github.com/utky.keys
+  echo "${SSH_PUBLIC_KEY}" > /home/${MAIN_USER}/.ssh/authorized_keys
   /usr/bin/chown ${MAIN_USER}:${MAIN_USER} /home/${MAIN_USER}/.ssh/authorized_keys
   /usr/bin/chmod 0600 /home/${MAIN_USER}/.ssh/authorized_keys
 
@@ -101,12 +85,29 @@ cat <<-EOF > "/mnt${CONFIG_SCRIPT}"
   
   echo "workspace" > /etc/hostname
   echo "127.0.1.1	workspace.localdomain	workspace" >> /etc/hosts 
-  mkinitcpio -P
-  
-  grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=grub
+
+  pacman -S --noconfirm grub efibootmgr 
+  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=boot --removable
   grub-mkconfig -o /boot/grub/grub.cfg
+
+  ls -l /boot/EFI/boot/bootx64.efi
+
+  systemctl enable systemd-networkd.service
+  systemctl enable systemd-resolved.service
+  ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+  echo '[Match]' > /etc/systemd/network/20-wired.network
+  echo 'Name=en*' >> /etc/systemd/network/20-wired.network
+  echo '' >> /etc/systemd/network/20-wired.network
+  echo '[Network]' >> /etc/systemd/network/20-wired.network
+  echo 'DHCP=yes' >> /etc/systemd/network/20-wired.network
+  cat /etc/systemd/network/20-wired.network
+
 EOF
 
-arch-chroot /mnt /mnt${CONFIG_SCRIPT}
+chmod 755 /mnt${CONFIG_SCRIPT}
+arch-chroot /mnt ${CONFIG_SCRIPT}
 
-systemctl reboot
+echo ">>>> install-base.sh: Completing installation.."
+/usr/bin/sleep 3
+/usr/bin/umount -R /mnt
